220d7e984f730f2469a700315493aedd3da3e073
Alexander Nicolas Szyrej <aszyrej@ws5.labo7>
2013-11-07 17:59:53 -0300

    deadlock solved y un tester incompleto

--- entregable/backend-multi/RWLock.h
+++ entregable/backend-multi/RWLock.h
@@ -12,15 +12,15 @@ class RWLock {
         void wunlock();
 
     private:
-	sem_t rw_lock;
-	sem_t accessR;
-	sem_t accessW;
+		sem_t rw_lock;
+		sem_t accessR;
+		sem_t accessW;
 		
-	sem_t mutex_r;	
-	int readers;
+		sem_t mutex_r;	
+		int readers;
 
-	sem_t mutex_w;	
-	int writers;
+		sem_t mutex_w;	
+		int writers;
 };
 
 #endif
---
+++ entregable/backend-multi/testerRW.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "RWLock.h"
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	RWLock lock;
+	int variable_a_leer_escribir = 0;
+	int cant_lectores = 1
+	int cant_escritores = 1
+	
+	for (int i = 0; i < cant_lectores; i++);
+		pid_t pid = fork();
+		if (pid == 0) {
+			lector();
+			break;
+		}
+    }
+    if (pìd != 0){	
+	
+		for (int i = 0; i < cant_escritores; i++){
+			pid_t pid = fork();
+			if (pid == 0) {
+				escritor();
+				break;
+			}
+		}	
+	}
+	
+	if (pìd != 0){	
+	
+	
+		//~ sincronizar
+		//~ for each
+		wait(&status);
+	
+	
+	
+	}
+	
+	
+	return 0;
+}
